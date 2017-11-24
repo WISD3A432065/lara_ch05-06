661809da36c63eaaf07356c032c6b33e096de6b1
龔育晴 <ching85117@gmail.com>
2017-11-24 16:43:15 +0800

    練習二：提供學生查詢自己的某科成績

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//練習一
+//練習一：顯示學生的資料與成績
+/*
 Route::get('student/{student_no}',function($student_no){
     return "學號：".$student_no;
 });
 Route::get('student/{student_no}/score',function ($student_no){
     return '學號：'.$student_no.'的所有成績';
+});
+*/
+
+//練習二：提供學生查詢自己的某科成績
+Route::get('student/{student_no}/score/{subject}', function ($student_no,$subject){
+    return '學號：'.$student_no.'的'.$subject.'成績';
 });
