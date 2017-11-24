c814dce5bf07b94d0d5ab849c1bf4b2ef859f9d0
龔育晴 <ching85117@gmail.com>
2017-11-24 17:42:41 +0800

    練習九：StudentController內加入getStudentData及getStudentScore函數

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function getStudentData($student_no)
+    {
+        return view::make('score', ['student_no' => $student_no, 'subject' => null
+        ]);
+    }
+    public function getStudentScore($student_no, $subject = null){
+        return view::make('score', ['student_no' => $student_no, 'subject' => $subject
+        ]);
+    }
+}
