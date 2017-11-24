e78a585cbeaf9db02a49fa493c97418d76876daa
龔育晴 <ching85117@gmail.com>
2017-11-24 17:05:15 +0800

    練習六：將網址前套上student

--- routes/web.php
+++ routes/web.php
@@ -48,11 +48,25 @@ Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subje
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
  }) -> where(['student_no' => 's[0-9]{10}','subject' => '(chinese|english|math)']);
 */
+
 //練習五：將學號統一進行格式設定
-Route::pattern('student_no','s[0-9]{10}');
+/*
+ * Route::pattern('student_no','s[0-9]{10}');
 Route::get('student/{student_no}',function ($student_no){
     return '學號：'.$student_no;
 });
 Route::get('student/{student_no}/score/{subject?}', function ($student_no,$subject=null){
     return '學號：'.$student_no.'的'.((is_null($subject))?'所有科目':$subject).'成績';
  }) -> where(['subject' => '(chinese|english|math)']);
+ */
+
+//練習六：將網址前套上student
+Route::pattern('student_no','s[0-9]{10}');
+Route::group(['prefix' => 'student'],function(){
+    Route::get('{student_no}', function ($student_no) {
+        return '學號：' . $student_no;
+ });
+    Route::get('{student_no}/score/{subject?}', function ($student_no, $subject = null) {
+        return '學號：' . $student_no . '的' . ((is_null($subject)) ? '所有科目' : $subject) . '成績';
+ })->where(['subject' => '(chinese|english|math)']);
+ });
